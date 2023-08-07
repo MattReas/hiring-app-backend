9ecb364bb8f91a730bb3d055f109309172a5ee33
MattReas <mattreas52@gmail.com>
2023-08-06 22:11:31 -0500

    Fixed update interview template

--- src/data/template/interview-template.service.ts
+++ src/data/template/interview-template.service.ts
@@ -56,11 +56,42 @@ export class InterviewTemplateService {
         // return this.interviewTemplateRepository.save(newTemplate)
     }
 
-    async update(id: number, template: Partial<InterviewTemplate>): Promise<InterviewTemplate> {
-        await this.findOne(id) // will throw an error if the template doesn't exist
-        await this.interviewTemplateRepository.update(id, template)
-        return this.interviewTemplateRepository.findOneByOrFail({ id:id })
+    async update(id: number, templateData: Partial<InterviewTemplate>): Promise<InterviewTemplate> {
+        // Fetch the existing template with its questions
+        const template = await this.interviewTemplateRepository.findOne({where: { id: id },  relations: ['questions'] }); 
+    
+        if (!template) throw new NotFoundException(`Interview template with ID ${id} not found`);
+    
+        // Update template's simple fields
+        if (templateData.templateName) {
+            template.templateName = templateData.templateName;
+        }
+    
+        // For the purpose of comparison and deletion
+        const updatedQuestionIds = templateData.questions.map(q => q.id);
+    
+        // Remove questions not present in the update data
+        template.questions = template.questions.filter(question => updatedQuestionIds.includes(question.id));
+    
+        // Update or add questions
+        for (let questionData of templateData.questions) {
+            let question = template.questions.find(q => q.id === questionData.id);
+            if (question) {
+                // Update existing question
+                question.question = questionData.question;
+            } else {
+                // Add new question
+                const newQuestion = new InterviewQuestion();
+                newQuestion.question = questionData.question;
+                newQuestion.template = template; // Set relation
+                template.questions.push(newQuestion);
+            }
+        }
+    
+        // Save the template, cascading the changes to its questions
+        return await this.interviewTemplateRepository.save(template);
     }
+    
 
     async delete(id: number): Promise<void> {
         const result = await this.interviewTemplateRepository.delete(id)
